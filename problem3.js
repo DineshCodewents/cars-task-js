ef83a470591a1fd81ee9900077ba39eebd44191f
DineshCodewents <dinesh.codewents@gmail.com>
2023-10-18 19:29:11 +0530

    The modified problem3.js code added
    In this js doing export function. In this function 'inventory' will identifying the car model in the alphabeticall oredr using for loop and if condition these all in 'function sortcar'.

--- problem3.js
+++ problem3.js
@@ -1,4 +1,19 @@
-import { sortcar } from './testproblem3.mjs';
-
-const sortedcarmodels = sortcar();
-console.log(sortedcarmodels);
+function sortcar(inventory) {
+    const carModels = [];
+    const inventoryLength = inventory.length;
+    for (let i = 0; i < inventoryLength; i++) {
+      carModels.push(inventory[i].car_model);
+    }
+  
+    for (let i = 0; i < carModels.length - 1; i++) {
+      for (let j = i + 1; j < carModels.length; j++) {
+        if (carModels[i] > carModels[j]) {
+          [carModels[i], carModels[j]] = [carModels[j], carModels[i]];
+        }
+      }
+    }
+  
+    return carModels;
+  }
+  
+  module.exports = sortcar

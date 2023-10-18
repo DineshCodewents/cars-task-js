e38d50ffda12a6218eadc6abfaad1eceecc4d5bc
DineshCodewents <dinesh.codewents@gmail.com>
2023-10-18 19:49:05 +0530

    The modified testproblem3.js code added.
    In this file having inventory about cars unique id, car make, car model and year. here imports a function using require and in this having one const one is sortcar then console will prints.

--- test/testproblem3.js
+++ test/testproblem3.js
@@ -48,22 +48,6 @@ let inventory = [{"id":1,"car_make":"Lincoln","car_model":"Navigator","car_year"
 {"id":48,"car_make":"Dodge","car_model":"Magnum","car_year":2008},
 {"id":49,"car_make":"Chrysler","car_model":"Sebring","car_year":1996},
 {"id":50,"car_make":"Lincoln","car_model":"Town Car","car_year":1999}];
-function sortcar() {
-  const carModels = [];
-  const inventoryLength = inventory.length;
-  for (let i = 0; i < inventoryLength; i++) {
-    carModels.push(inventory[i].car_model);
-  }
-
-  for (let i = 0; i < carModels.length - 1; i++) {
-    for (let j = i + 1; j < carModels.length; j++) {
-      if (carModels[i] > carModels[j]) {
-        [carModels[i], carModels[j]] = [carModels[j], carModels[i]];
-      }
-    }
-  }
-
-  return carModels;
-}
-
-export { inventory, sortcar };
+const sortcar = require('../problem3.js');
+const sortedCarModels = sortcar(inventory);
+console.log(sortedCarModels);

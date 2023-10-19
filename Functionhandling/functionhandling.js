20ef357b7a0f9dbbfa72985a9c76bfa9c86f7f29
DineshCodewents <dinesh.codewents@gmail.com>
2023-10-19 20:53:40 +0530

    Added a functionhandling.js file.
    In this file having a function. function name is 'ValidateInventory' this includes if condition for inventory in array formate. Then forLoop used in this function for data validation purpose. After return inventory will performs. Finally this function will exports.

---
+++ Functionhandling/functionhandling.js
@@ -0,0 +1,20 @@
+function validateInventory(inventory) {
+    if (!Array.isArray(inventory)) {
+      throw new Error('Invalid inventory: Inventory must be an array.');
+    }
+    for (let i = 0; i < inventory.length; i++) {
+      const car = inventory[i];
+      const isValidCar =
+        typeof car === 'object' &&
+        typeof car.id === 'number' &&
+        Number.isInteger(car.id) &&
+        typeof car.car_make === 'string' &&
+        car.car_make.length > 0 &&
+        typeof car.car_model === 'string' &&
+        car.car_model.length > 0 &&
+        typeof car.car_year === 'number' || car.car_year === '' || (typeof car.car_year === 'string' && !isNaN(car.car_year))
+    }
+    return inventory;
+  }
+
+  module.exports= validateInventory;

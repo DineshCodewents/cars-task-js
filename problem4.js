8130c881c0196cd14311a6e15af6ae0089d4547a
DineshCodewents <dinesh.codewents@gmail.com>
2023-10-19 21:51:26 +0530

    modified problem4.js
    Removed a 'validateInventory'function . Added a import using const 'validateInventory'. created a function 'getAllCarYear' years form inventorty.forloop using 'inventory.length' to identify the count of cars and that years. changed all variable names in camelCase method.

--- problem4.js
+++ problem4.js
@@ -1,45 +1,18 @@
-function validateInventory(inventory) {
-  if (!Array.isArray(inventory)) {
-    throw new Error('Invalid inventory: Inventory must be an array.');
-  }
-
-  for (let i = 0; i < inventory.length; i++) {
-    const car = inventory[i];
-    const isValidCar =
-      typeof car === 'object' &&
-      typeof car.id === 'number' &&
-      Number.isInteger(car.id) &&
-      typeof car.car_make === 'string' &&
-      car.car_make.length > 0 &&
-      typeof car.car_model === 'string' &&
-      car.car_model.length > 0 &&
-      (typeof car.car_year === 'number' || (typeof car.car_year === 'string' && !isNaN(parseInt(car.car_year))));
-
-    if (!isValidCar) {
-      throw new Error(`Invalid inventory item at index ${i}: 
-        - id must be a positive integer,
-        - car_make and car_model must be strings with length greater than 3,
-        - car_year must be a positive integer or a valid numeric string.`);
-    }
-  }
-
-  return inventory;
-}
-
-function getallcaryear(inventory) {
+const validateInventory = require('../Javascript Cars/Functionhandling/functionhandling');
+function getAllCarYear(inventory) {
   try {
     validateInventory(inventory);
-    const caryears = [];
+    const carYears = [];
     for (let i = 0; i < inventory.length; i++) {
-      caryears.push(inventory[i].car_year);
+      carYears.push(inventory[i].car_year);
     }
     const carCount = inventory.length;
     const countMessage = `Total cars in inventory: ${carCount}`;
-    return { years: caryears, count: countMessage };
+    return { years: carYears, count: countMessage };
   } catch (error) {
     console.error('Error:', error.message);
     return null;
   }
 }
 
-module.exports = getallcaryear;
+module.exports = getAllCarYear;

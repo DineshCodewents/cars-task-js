3c17d65da5b8565a139688d967a9c4a7b984b6d6
DineshCodewents <dinesh.codewents@gmail.com>
2023-10-19 13:59:26 +0530

    Exception handling to problem3.js code added.
    Added a code of exception handling for inventory for specificly to 'carid, car model, car make and car year 'using function 'ValidateInventory'and 'sortcar'.

--- problem3.js
+++ problem3.js
@@ -1,19 +1,42 @@
-function sortcar(inventory) {
-    const carModels = [];
-    const inventoryLength = inventory.length;
-    for (let i = 0; i < inventoryLength; i++) {
-      carModels.push(inventory[i].car_model);
-    }
-  
-    for (let i = 0; i < carModels.length - 1; i++) {
-      for (let j = i + 1; j < carModels.length; j++) {
-        if (carModels[i] > carModels[j]) {
-          [carModels[i], carModels[j]] = [carModels[j], carModels[i]];
-        }
-      }
-    }
-  
-    return carModels;
+function validateInventory(inventory) {
+  if (!Array.isArray(inventory)) {
+    throw new Error('Invalid inventory: Inventory must be an array.');
   }
-  
-  module.exports = sortcar
+
+  for (let i = 0; i < inventory.length; i++) {
+    const car = inventory[i];
+    const isValidCar =
+      typeof car === 'object' &&
+      typeof car.id === 'number' &&
+      Number.isInteger(car.id) &&
+      typeof car.car_make === 'string' &&
+      car.car_make.length > 0&&
+      typeof car.car_model === 'string' &&
+      car.car_model.length > 0&&
+      (typeof car.car_year === 'number' || (typeof car.car_year === 'string' && !isNaN(parseInt(car.car_year))));
+
+    if (!isValidCar) {
+      throw new Error(`Invalid inventory item at index ${i}: 
+        - id must be a positive integer,
+        - car_make and car_model must be strings with length greater than 3,
+        - car_year must be a positive integer or a valid numeric string.`);
+    }
+  }
+  return inventory;
+}
+function sortcar(inventory) {
+  try {
+    validateInventory(inventory);
+    const carModels = [];
+    for (let i = 0; i < inventory.length; i++) {
+      const car = inventory[i];
+      carModels.push(car.car_model);
+    }
+    carModels.sort();
+    return carModels;
+  } catch (error) {
+    console.error('Error:', error.message);
+    return null;
+  }
+}
+module.exports = sortcar;

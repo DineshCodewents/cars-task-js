36eb03bf11c54c42e51ba1ee745e4587843a934e
DineshCodewents <dinesh.codewents@gmail.com>
2023-10-19 21:59:56 +0530

    modified problem6.js added
    Removed a 'validateInventory'function . Added a import using const 'validateInventory'. created a function 'getBmwAndAudiCars' to identify the bmw and audi cars in inventory .forloop using 'inventory.length'.If condition says inventory car_make= bmw and car_make=audi in retrun. changed all variable names in camelCase method.

--- problem6.js
+++ problem6.js
@@ -1,30 +1,5 @@
-function validateInventory(inventory) {
-  if (!Array.isArray(inventory)) {
-    throw new Error('Invalid inventory: Inventory must be an array.');
-  }
-
-  for (let i = 0; i < inventory.length; i++) {
-    const car = inventory[i];
-    const isValidCar =
-      typeof car === 'object' &&
-      typeof car.id === 'number' &&
-      Number.isInteger(car.id) &&
-      typeof car.car_make === 'string' &&
-      car.car_make.length > 0 &&
-      typeof car.car_model === 'string' &&
-      car.car_model.length > 0 &&
-      (typeof car.car_year === 'number' || (typeof car.car_year === 'string' && !isNaN(parseInt(car.car_year))));
-
-    if (!isValidCar) {
-      throw new Error(`Invalid inventory item at index ${i}: 
-        - id must be a positive integer,
-        - car_make and car_model must be strings with length greater than 3,
-        - car_year must be a positive integer or a valid numeric string.`);
-    }
-  }
-  return inventory;
-}
-function getBMWandAudiCars(inventory) {
+const validateInventory = require('../Javascript Cars/Functionhandling/functionhandling');
+function getBMWAndAudiCars(inventory) {
   try {
     validateInventory(inventory);
     const BMWAndAudiCars = [];
@@ -39,4 +14,4 @@ function getBMWandAudiCars(inventory) {
     return null;
   }
 }
-module.exports = getBMWandAudiCars;
+module.exports = getBMWAndAudiCars;

6019e4eb2786d5dac0c2052ff270dc19dc0b3a81
DineshCodewents <dinesh.codewents@gmail.com>
2023-10-19 21:45:42 +0530

    modified problem3.js
    Removed a 'validateInventory'function . Added a import using const 'validateInventory'. created a function 'sortCar' for arraging car_model in alphabetical order form inventorty. 'Inventory.sort' take a and b this two in 'toLowerCase' this two are compare using localCompare.the result will stored in let veriable 'result'.for condition using to print a formate of array. changed all variable names in camelCase method.

--- problem3.js
+++ problem3.js
@@ -1,42 +1,17 @@
-function validateInventory(inventory) {
-  if (!Array.isArray(inventory)) {
-    throw new Error('Invalid inventory: Inventory must be an array.');
-  }
-
-  for (let i = 0; i < inventory.length; i++) {
-    const car = inventory[i];
-    const isValidCar =
-      typeof car === 'object' &&
-      typeof car.id === 'number' &&
-      Number.isInteger(car.id) &&
-      typeof car.car_make === 'string' &&
-      car.car_make.length > 0&&
-      typeof car.car_model === 'string' &&
-      car.car_model.length > 0&&
-      (typeof car.car_year === 'number' || (typeof car.car_year === 'string' && !isNaN(parseInt(car.car_year))));
-
-    if (!isValidCar) {
-      throw new Error(`Invalid inventory item at index ${i}: 
-        - id must be a positive integer,
-        - car_make and car_model must be strings with length greater than 3,
-        - car_year must be a positive integer or a valid numeric string.`);
-    }
-  }
-  return inventory;
-}
-function sortcar(inventory) {
+const validateInventory = require('../Javascript Cars/Functionhandling/functionhandling');
+function sortCar(inventory) {
   try {
     validateInventory(inventory);
-    const carModels = [];
+    inventory.sort((a, b) => a.car_model.toLowerCase().localeCompare(b.car_model.toLowerCase()));
+    let result = '';
     for (let i = 0; i < inventory.length; i++) {
       const car = inventory[i];
-      carModels.push(car.car_model);
+      result += `id: ${car.id}, car_make: ${car.car_make}, car_model: ${car.car_model}, car_year: ${car.car_year}\n`;
     }
-    carModels.sort();
-    return carModels;
+    return result;
   } catch (error) {
     console.error('Error:', error.message);
     return null;
   }
 }
-module.exports = sortcar;
+module.exports = sortCar;

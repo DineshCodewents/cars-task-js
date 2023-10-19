43654322cd64023d3fa6e6c5681da2cbc1cf9545
DineshCodewents <dinesh.codewents@gmail.com>
2023-10-19 14:01:23 +0530

    Exception handling to problem5.js code added.
    Added a code of exception handling for inventory for specificly to 'carid, car model, car make and car year 'using function 'ValidateInventory'and 'getallcar and getolder'.

--- problem5.js
+++ problem5.js
@@ -1,20 +1,59 @@
-function getallcar(inventory) {
-    const carYears = [];
+function validateInventory(inventory) {
+    if (!Array.isArray(inventory)) {
+      throw new Error('Invalid inventory: Inventory must be an array.');
+    }
+  
     for (let i = 0; i < inventory.length; i++) {
+      const car = inventory[i];
+      const isValidCar =
+        typeof car === 'object' &&
+        typeof car.id === 'number' &&
+        Number.isInteger(car.id) &&
+        typeof car.car_make === 'string' &&
+        car.car_make.length > 0 &&
+        typeof car.car_model === 'string' &&
+        car.car_model.length > 0 &&
+        (typeof car.car_year === 'number' || (typeof car.car_year === 'string' && !isNaN(parseInt(car.car_year))));
+  
+      if (!isValidCar) {
+        throw new Error(`Invalid inventory item at index ${i}: 
+          - id must be a positive integer,
+          - car_make and car_model must be strings with length greater than 3,
+          - car_year must be a positive integer or a valid numeric string.`);
+      }
+    }
+    return inventory;
+  }
+  function getallcar(inventory) {
+    try {
+      validateInventory(inventory);
+      const carYears = [];
+      for (let i = 0; i < inventory.length; i++) {
         carYears.push(inventory[i].car_year);
+      }
+      return carYears;
+    } catch (error) {
+      console.error('Error in getallcar:', error.message);
+      return null;
     }
-    return carYears;
-}
-
-function getolder(caryears) {
-    const oldercars = [];
-    for (let i = 0; i < caryears.length; i++) {
+  }
+  function getolder(caryears) {
+    try {
+      if (!Array.isArray(caryears)) {
+        throw new Error('Invalid input: car years must be an array.');
+      }
+  
+      const oldercars = [];
+      for (let i = 0; i < caryears.length; i++) {
         if (caryears[i] < 2000) {
-            oldercars.push(caryears[i]);
+          oldercars.push(caryears[i]);
         }
+      }
+      return oldercars;
+    } catch (error) {
+      console.error('Error in getolder:', error.message);
+      return null;
     }
-    return oldercars;
-}
-module.exports.getallcar = getallcar;
-module.exports.getolder = getolder;
-
+  }
+  module.exports.getallcar = getallcar;
+  module.exports.getolder = getolder;  

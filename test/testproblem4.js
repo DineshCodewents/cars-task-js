7b3b35a5e36c6acf65c41166abfe23b830c8e9e6
DineshCodewents <dinesh.codewents@gmail.com>
2023-10-20 14:31:46 +0530

    modified testproblem4.js
    removed inventory array . then added a new line for import of dataSet file.

--- test/testproblem4.js
+++ test/testproblem4.js
@@ -1,5 +1,6 @@
 const inventory=require('../dataSet/dataSet.js')
 const getAllCarYear = require('../problem4.js');
+
 const carYears = getAllCarYear(inventory);
 console.log('Car years array:', carYears);
 

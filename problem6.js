23567a1da9a5123102e6121ac1ed6ae5032312f2
DineshCodewents <dinesh.codewents@gmail.com>
2023-10-20 14:41:00 +0530

    modified problem6.js
    changed inventory file driectory.

--- problem6.js
+++ problem6.js
@@ -1,4 +1,4 @@
-const validateInventory = require('../Javascript Cars/Functionhandling/functionhandling');
+const validateInventory = require('./functionHandling/functionHandling');
 function getBMWAndAudiCars(inventory) {
   try {
     validateInventory(inventory);

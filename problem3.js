fb752c17215b2fbbee80e43b92ce8609e5731867
DineshCodewents <dinesh.codewents@gmail.com>
2023-10-20 10:55:13 +0530

    modified printing formate of problem3.js
    Removed the old output format of string method to array method.

--- problem3.js
+++ problem3.js
@@ -3,15 +3,11 @@ function sortCar(inventory) {
   try {
     validateInventory(inventory);
     inventory.sort((a, b) => a.car_model.toLowerCase().localeCompare(b.car_model.toLowerCase()));
-    let result = '';
-    for (let i = 0; i < inventory.length; i++) {
-      const car = inventory[i];
-      result += `id: ${car.id}, car_make: ${car.car_make}, car_model: ${car.car_model}, car_year: ${car.car_year}\n`;
-    }
-    return result;
+    return inventory;
   } catch (error) {
     console.error('Error:', error.message);
     return null;
   }
 }
+
 module.exports = sortCar;

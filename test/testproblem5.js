b0e3c0d06468ce7175dc48db02b562d5fd1ef552
DineshCodewents <dinesh.codewents@gmail.com>
2023-10-20 15:53:26 +0530

    files name changed into testProblem

--- test/testproblem5.js
+++ test/testproblem5.js
@@ -1,5 +1,6 @@
 const inventory = require('../dataSet/dataSet.js');
 const getOlderCars = require('../problem5.js');
+
 const olderCars = getOlderCars(inventory);
 console.log('Older cars:', olderCars);
 
